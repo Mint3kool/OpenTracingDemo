eb581fcb5d2291c04bbb601dd5735c5e35957a40
Tay Shen <stay9@vt.edu>
2020-10-26 15:45:09 -0400

    working version with jaeger

--- src/main/java/com/example/openTracing/rest/TracingResource.java
+++ src/main/java/com/example/openTracing/rest/TracingResource.java
@@ -19,6 +19,7 @@ import com.example.openTracing.Producer;
 import com.example.openTracing.util.HttpHeadersExtract;
 import com.example.openTracing.util.RequestBuilderCarrier;
 
+import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
@@ -34,7 +35,12 @@ import java.util.Map;
 import javax.jms.Message;
 
 /**
- * Note: jmsTemplate is used for completely synchronous jms client calls
+ * Notes: 
+ * 
+ * JmsTemplate is used for completely synchronous jms client calls
+ * 
+ * I've also intentionally grouped all the tracer logic as much as possible here to clearly show
+ * the necessary steps when initializing and sending traces
  */
 @Service
 @Component
@@ -70,20 +76,19 @@ public class TracingResource {
 
 	public void sendCustomMessage(String queue, String message) {
 
+//		AmqpTemplate amqp = ctx.getBean(AmqpTemplate.class);
 		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
 		jms.convertAndSend(queue, message);
 	}
 
-	@RequestMapping(value = "/externalRequest", method = RequestMethod.POST)
-	public void externalRequest() throws IOException {
+	@RequestMapping(value = "/nestedExternalRequest", method = RequestMethod.POST)
+	public void nestedExternalRequest() throws IOException {
 		Tracer t = GlobalTracer.get();
 
-//		http://localhost:8081/api/apiTrace
-
 		HttpUrl url = new HttpUrl.Builder().scheme("http").host("localhost").port(8081).addPathSegment("api")
-				.addPathSegment("apiTrace").addQueryParameter("param", "value").build();
+				.addPathSegment("nestedApiTrace").addQueryParameter("param", "value").build();
 
-		Span newSpan = t.buildSpan("external_span").start();
+		Span newSpan = t.buildSpan("top_external_span").start();
 
 		t.scopeManager().activate(newSpan);
 
@@ -91,7 +96,7 @@ public class TracingResource {
 
 		Request.Builder requestBuilder = new Request.Builder().url(url);
 
-		Tags.SPAN_KIND.set(t.activeSpan(), Tags.SPAN_KIND_CLIENT);
+		Tags.SPAN_KIND.set(t.activeSpan(), Tags.SPAN_KIND_SERVER);
 		Tags.HTTP_METHOD.set(t.activeSpan(), "GET");
 		Tags.HTTP_URL.set(t.activeSpan(), url.toString());
 		t.inject(t.activeSpan().context(), Format.Builtin.HTTP_HEADERS, new RequestBuilderCarrier(requestBuilder));
@@ -104,6 +109,36 @@ public class TracingResource {
 
 		newSpan.finish();
 	}
+	
+	@RequestMapping(value = "/finishedExternalRequest", method = RequestMethod.POST)
+	public void finishedExternalRequest() throws IOException {
+		Tracer t = GlobalTracer.get();
+
+		HttpUrl url = new HttpUrl.Builder().scheme("http").host("localhost").port(8081).addPathSegment("api")
+				.addPathSegment("finishedApiTrace").addQueryParameter("param", "value").build();
+
+		Span newSpan = t.buildSpan("finished_external_span").start();
+
+		Scope s = t.scopeManager().activate(newSpan);//, true);
+
+		newSpan.setTag("more_baggage", "super_bags");
+		
+		Request.Builder requestBuilder = new Request.Builder().url(url);
+
+		Tags.SPAN_KIND.set(t.activeSpan(), Tags.SPAN_KIND_SERVER);
+		Tags.HTTP_METHOD.set(t.activeSpan(), "GET");
+		Tags.HTTP_URL.set(t.activeSpan(), url.toString());
+		t.inject(t.activeSpan().context(), Format.Builtin.HTTP_HEADERS, new RequestBuilderCarrier(requestBuilder));
+
+		Request r1 = requestBuilder.build();
+		
+		newSpan.finish();
+
+		Response response = client.newCall(r1).execute();
+
+		Tags.HTTP_STATUS.set(t.activeSpan(), response.code());
+	}
+	
 
 	public void sendCustomObjectMessage(String queue, Object o) {
 		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
